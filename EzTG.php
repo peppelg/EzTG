0e472b87c75cab07234d6d85af4409f31a92ef5a
Giuseppe <me@peppelg.space>
2018-07-07 19:14:18 +0200

    Magic json payload,.,
    Sùrreale non altro .

--- EzTG.php
+++ EzTG.php
@@ -5,6 +5,7 @@ class EzTGException extends Exception {
 class EzTG {
   private $settings;
   private $offset;
+  private $json_payload;
   public function __construct($settings, $base = false) {
     $this->settings = array_merge(array(
       'endpoint' => 'https://api.telegram.org',
@@ -13,16 +14,23 @@ class EzTG {
         echo 'no callback' . PHP_EOL;
       },
       'objects' => true,
-      'throw_telegram_errors' => true
+      'throw_telegram_errors' => true,
+      'magic_json_payload' => true
     ), $settings);
     if ($base !== false)
       return true;
     if (!is_callable($this->settings['callback']))
       $this->error('Invalid callback.', true);
     if (php_sapi_name() === 'cli') {
+      $this->settings['magic_json_payload'] = false;
       $this->offset = -1;
       $this->get_updates();
     } else {
+      if ($this->settings['magic_json_payload'] === true) {
+        ob_start();
+        $this->json_payload = false;
+        register_shutdown_function(array($this, 'send_json_payload'));
+      }
       $this->processUpdate(json_decode(file_get_contents('php://input')));
     }
   }
@@ -81,6 +89,23 @@ class EzTG {
   public function __call($name, $arguments) {
     if (!isset($arguments[0]))
       $arguments[0] = array();
+    if (!isset($arguments[1]))
+      $arguments[1] = true;
+    if ($this->settings['magic_json_payload'] === true and $arguments[1] === true) {
+      if ($this->json_payload === false) {
+        $arguments[0]['method'] = $name;
+        $this->json_payload = $arguments[0];
+        return 'json_payloaded'; //xd
+      } elseif(is_array($this->json_payload)) {
+        $old_payload = $this->json_payload;
+        $arguments[0]['method'] = $name;
+        $this->json_payload = $arguments[0];
+        $name = $old_payload['method'];
+        $arguments[0] = $old_payload;
+        unset($arguments[0]['method']);
+        unset($old_payload);
+      }
+    }
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $this->settings['endpoint'] . '/bot' . $this->settings['token'] . '/' . urlencode($name));
     curl_setopt($ch, CURLOPT_POST, true);
@@ -103,6 +128,15 @@ class EzTG {
         return $result['result'];
     }
     return $this->error('Unknown error', false);
+  }
+  public function send_json_payload() {
+    if (is_array($this->json_payload)) {
+      ob_end_clean();
+      echo json_encode($this->json_payload);
+      header('Content-Type: application/json');
+      ob_end_flush();
+      return true;
+    }
   }
 }
 class EzTGKeyboard {
